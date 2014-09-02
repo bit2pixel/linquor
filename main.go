1a1e8c31d9e0da82bacc9b5b13ff585e95aa0c93
Renan Cakirerk <renan@redbeacon.com>
2014-09-01 19:59:46 -0700

    It's now a web server

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import "net/http"
+
 func main() {
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8000", nil)
 	println("hello kitty!")
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello Kitty!"))
 }
